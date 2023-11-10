949966438020056abdd1a90f00de843a54b04cd7
Patrick <lipatrick093@gmail.com>
2023-11-10 12:43:18 -0500

    submit button, local storage, quiz working

--- script.js
+++ script.js
@@ -2,63 +2,106 @@
 var startButton = document.getElementById("startbutton");
 var startBox = document.getElementById("start-box");
 var quizBox = document.getElementById("quiz");
+const quizEl = document.getElementById("questions");
+var highscoreBox = document.getElementById("highscore");
+var submitButton = document.getElementById("submit");
 var timer = document.getElementById("timer");
-var choicesContainer = document.getElementById("choices");
+var choicesEl = document.getElementById("choices");
 var startTime = 60;
-var currentQuestion;
+let currentQuestion = 0;
+var resultElement = document.getElementById("result");
+var timerInterval;
+var allScores= JSON.parse(localStorage.getItem("savedData")) || [];
+
 
 var questionArr = [
-    {
-        question: 'What are JavaScript Data Types?',
-        choices: ['Number','String','Object','Boolean'],
-        answer: 3
-    },
-    {
-        question: '1+2',
-        choices: [0,1,2,3],
-        answer: 3
-    }
-]
+  {
+    question: "What are JavaScript Data Types?",
+    choices: ["Number", "String", "Object", "Boolean", "All of the above"],
+    answer: "All of the above",
+  },
+  {
+    question: "1+2",
+    choices: [0, 1, 2, 3],
+    answer: 3,
+  },
+];
 
 function startQuiz() {
-    // hide the startbox
-startBox.classList.add("hide");
-quizBox.classList.remove("hide");
-    // show the quiz container
+  // hide the startbox
+  startBox.classList.add("hide");
+  quizBox.classList.remove("hide");
+  // show the quiz container
 
-    // start timer
-var timerInterval = setInterval(() => {
+  // start timer
+  timerInterval = setInterval(() => {
     startTime--;
     timer.textContent = startTime;
-}, 1000);
-    // show question
-}
-function showQuestion()
-{
-    //if quiz is started
-    //then show question text
-    //and show choices for each question
-    var questionText = document.getElementById("questions");
-    questionText.textContent = questionArr[currentQuestion].question;
-    
-    var choiceText = document.querySelectorAll("#choices");
-    choiceText.forEach((choice,index) => { choice.textContent = 
-    questions[currentQuestion].choices[index]});
-    
-}
-function selectAnswer()
-{
-    //given box is created
-    //AND has a clickevent tied
-    //if choice is clicked
-    
-    // THEN check if its correct
-    // if its correct, add score and change question answer
-    // ELSE minus startTime
+  }, 1000);
+  // show question
+  showQuestion();
+
+  if (startTime === 0) {
+    console.log("times up");
+    endQuiz();
+  }
 }
 
+function showQuestion() {
+  quizEl.textContent = questionArr[currentQuestion].question;
+  choicesEl.innerHTML = "";
+  //if quiz is started
+  //then show question text
+  //and show choices for each question
+  questionArr[currentQuestion].choices.forEach((choice, index) => {
+    const choiceButton = document.createElement("button");
+    choiceButton.textContent = choice;
+    choiceButton.addEventListener("click", checkAnswer);
+    choicesEl.appendChild(choiceButton);
+  });
+  // choiceText.forEach((choice,index) => { choice.textContent =
+  // questions[currentQuestion].choices[index]});
+  // choiceText.forEach((choice, index) => {document.createElement('button')}
+  // );
+}
+
+
+function checkAnswer() {
+  //  stopTimer();
+  // Clear answer choices
+  if (this.textContent !== questionArr[currentQuestion].answer) {
+    resultElement.textContent = "Incorrect!";
+    startTime -= 5;
+    timer.textContent = startTime;
+  } else {
+    resultElement.textContent = "Correct!";
+    // score = score + 1;
+  }
+  currentQuestion++;
+
+  if (currentQuestion === questionArr.length) {
+    endQuiz();
+  } else {
+    showQuestion();
+  }
+}
+
+function endQuiz() {
+  clearInterval(timerInterval);
+  quizBox.classList.add("hide");
+  highscoreBox.classList.remove("hide");
+}
+function submitScore() {
+ var savedData = {
+    initials: document.getElementById("initials").value,
+    score: startTime
+ }
+ allScores.push(savedData);
+
+ localStorage.setItem("savedData",JSON.stringify(allScores));
+
+}
 // declare a function to show question
 
 startButton.addEventListener("click", startQuiz);
-
-choicesContainer.addEventListener("click",selectAnswer);
+submitButton.addEventListener("click",submitScore);
